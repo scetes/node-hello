634df0e0280847fe05b76640192d35a9370250c9
scetes <scetermacek@gmail.com>
2015-12-03 20:19:20 -0500

    tweaked the nodejs app

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ var express = require("express"),
 // Config
 
 app.get('/api', function (req, res) {
-  res.send('hello from docker in Google Container Engine');
+  res.send('hello from docker');
 });
 
 // Launch server
 
 app.listen(8088);
 
-console.log("node app listening on port 8088")
+console.log("node app listening")
