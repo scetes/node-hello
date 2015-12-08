3e070d9e32417d9b4cc89c35ebaa496de6b19fd7
scetes <scetermacek@gmail.com>
2015-12-07 21:51:14 -0500

    Create README.md

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ var express = require("express"),
 // Config
 
 app.get('/api', function (req, res) {
-  res.send('hello from docker');
+  res.send('hello from dockeroo');
 });
 
 // Launch server
 
 app.listen(8088);
 
-console.log("node app listening")
+console.log("node app listening");
